e76901a7e312e56d63fcb4d1e7b37955b1d66c5c
bjaehyun <bjaehyun@amazon.com>
2021-04-30 21:22:02 +0900

    userstack: add developer group and add user method

--- lib/user-stack.ts
+++ lib/user-stack.ts
@@ -6,51 +6,50 @@ export class UserStack extends cdk.Stack {
     super(scope, id, props);
 
     // DeveloperGroup
-    const devGroup = new iam.Group(this, 'Developer', {
+    const devGroup = new iam.Group(this, 'Group-Developer', {
       groupName: 'Developer',
-    });
-
-    // CdkPolicy
-    const developerPolicy = new iam.ManagedPolicy(this, 'DeveloperPolicy', {
-      managedPolicyName: 'DeveloperPolicy',
-      statements: [
-        new iam.PolicyStatement({
-          actions: [
-            'cloudformation:*'
-          ],
-          resources: [
-            '*'
-          ]
-        }),
-        new iam.PolicyStatement({
-          actions: [
-            's3:*'
-          ],
-          resources: [
-            'arn:aws:s3:::cdktoolkit-stagingbucket-*'
-          ]
-        }),
-        new iam.PolicyStatement({
-          actions: [
-            '*'
-          ],
-          resources: [
-            '*'
-          ],
-          conditions: {
-              'ForAnyValue:StringEquals': {
-                'aws:CalledVia': [
-                  "cloudformation.amazonaws.com"
-                ]
-              }
-            }
-        }),
+      managedPolicies: [
+        iam.ManagedPolicy.fromManagedPolicyArn(this, 'PowerUserAccess', 'arn:aws:iam::aws:policy/PowerUserAccess'),
+        iam.ManagedPolicy.fromManagedPolicyArn(this, 'IAMFullAccess', 'arn:aws:iam::aws:policy/IAMFullAccess')
       ]
     });
-    developerPolicy.attachToGroup(devGroup)
-
-    // User
 
     
+    
   }
+
+  protected addUser(userName: string, group: iam.Group) {
+
+    let initialPassword = '';
+    while(!this.validPassword(initialPassword)) {
+      initialPassword = '#'+(Math.random()*0xFFFFFF<<0).toString(16)+(Math.random()*0xFFFFFF<<0).toString(16).toUpperCase();
+      console.log(initialPassword);
+    }
+    //const initialPassword = 'Password#3';
+    const password = cdk.SecretValue.plainText(initialPassword);
+
+    const user = new iam.User(this, `User-${userName}`, {
+      userName: userName,
+      password: password,
+      passwordResetRequired: true,
+      groups: [
+        group
+      ],
+    });
+
+    new cdk.CfnOutput(this, `Output-Username-${userName}`, {
+      value: userName});
+
+    new cdk.CfnOutput(this, `Output-Password-${userName}`, {
+      value: password.toString()});
+
+  }
+
+  private validPassword(password: string) {
+    // have at least one uppercase letter
+    // have at least one number
+    // have at least one symbol
+    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\^$*.\[\]{}\(\)?\-“!@#%&\/,><\’:;|_~`])\S{8,99}$/.test(password)
+  }
+
 }
